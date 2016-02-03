03eb204c920bd01f45c98a8996b9378903a17280
nikeshrathod <nikesh.rathod@synerzip.com>
2016-02-03 15:56:16 +0530

    add post for todos

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id : 1,
-	description : "meet for lunch",
-	completed : false
-},{
-	id : 2,
-	description : "going for muv",
-	completed : false
-},{
-	id : 3,
-	description : "happy",
-	completed : true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res){
 	res.send('to-do api root');
@@ -40,6 +33,16 @@ app.get('/todos/:id',function (req,res){
 		res.status(404).send();
 	}
 });
+
+
+//POST /todos
+app.post('/todos', function (req,res) {
+	var body = req.body;
+	body.id = todoNextId++;
+	todos.push(body);
+	res.json(body);
+});
+
 
 app.listen(PORT, function (){
 	console.log('express listening on port' + PORT);
