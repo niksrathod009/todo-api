acc9ea4004c803b1ed1d50f468415d2aaadee96e
nikeshrathod <nikesh.rathod@synerzip.com>
2016-02-12 17:10:10 +0530

    add variable-playground area

---
+++ variable-playground.js
@@ -0,0 +1,21 @@
+// var person = {
+// 	name: 'nik',
+// 	age : 21
+// };
+
+// function updatePerson (obj) {
+// 	obj.age : 26
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var grades = [15,88];
+
+function addGrades (grades) {
+	grades.push(55);
+	debugger;
+}
+
+addGrades(grades);
+console.log(grades);
